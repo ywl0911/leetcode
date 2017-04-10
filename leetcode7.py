a61c181cd15bd8a0c0958213dbae8e1d2486fc7d
yuwenli <yuwenli@gridsum.com>
2017-04-10 15:20:35 +0800

    1

--- leetcode7.py
+++ leetcode7.py
@@ -1,42 +1,21 @@
-import copy
-
-import numpy as np
 class Solution(object):
-    def convert(self, s, numRows):
+    def reverse(self, x):
+        if x<-2**31 or x>2**31:
+            return 0
         """
-        :type s: str
-        :type numRows: int
-        :rtype: str
+        :type x: int
+        :rtype: int
         """
-        if numRows==1:
-            return s
-        result = []
-        total = len(s)
-        for i in range(numRows):
-            temp_result = []
-            n = 0
+        flag=1
+        if x<0:
+            x=-x
+            flag=-1
 
-            while (2 * numRows - 2) * n + i < total:
-                temp_result.append((2 * numRows - 2) * n + i)
-                n = n + 1
-            result.append(temp_result)
-        pass
-        result_copy = copy.deepcopy(result)
-        cha = 2 * numRows - 4
-        for i in range(len(result_copy)):
-            if i in [0, len(result_copy) - 1]:
-                continue
+        x_str=list(str(x))
+        x_str.reverse()
+        a= int(''.join(x_str))*flag
+        if a<-2**31 or a>2**31:
+            return 0
+        return a
 
-            for j in range(len(result_copy[i])):
-                if result_copy[i][j] + cha < total:
-                    result[i].insert(2*j + 1, result_copy[i][j] + cha)
-            cha = cha - 2
-        pass
 
-        result_last=[]
-        for i in result:
-            result_last+=i
-        pass
-        return ''.join(np.array(list(s))[np.array(result_last)])
-s = Solution()
-print((s.convert('123456789012345', 3)))
