fb824c1d0ae09acbe702cfe5175824a1c2ca62b9
yuwenli <841843777@qq.com>
2017-07-31 22:16:55 +0800

    dfs深度优先遍历，回溯，递归

---
+++ leetcode39.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def combinationSum(self, candidates, target):
+        """
+        :type candidates: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        self.result = []
+        candidates = sorted(candidates)
+        self.dfs(candidates, [], target, 0)
+
+        return self.result
+
+    def dfs(self, candidates, read_list, target, last):
+        if target == 0:
+            self.result.append(read_list[:])  # a[:]是传值，a是传址
+        if target < candidates[0]:
+            return
+
+        for i in candidates:
+            if i > target:
+                return
+            if i < last:
+                continue
+            read_list.append(i)
+            self.dfs(candidates, read_list, target - i, i)
+            read_list.pop()
+
+
+b = Solution()
+print(b.combinationSum([2, 3, 6, 7], 7))
