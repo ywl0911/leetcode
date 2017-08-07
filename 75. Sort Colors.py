1caa6c7628bbff8bdc82603c558f8fa84f3955ef
yuwenli <yuwenli@gridsum.com>
2017-08-07 14:56:23 +0800

    排序

---
+++ 75. Sort Colors.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def sortColors(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        if nums == []:
+            return nums
+
+        nums.sort()
