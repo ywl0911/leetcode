af1c7855dd4c6655f522925e70eb88a8f3f5110e
yuwenli <841843777@qq.com>
2017-08-12 11:29:52 +0800

    dfs求子集

--- 90. Subsets II.py
+++ 90. Subsets II.py
@@ -3,3 +3,17 @@ class Solution:
     # @return a list of lists of integer
     # a dfs problem
     def subsetsWithDup(self, S):
+        S.sort()
+        ready_list = []
+        self.result = []
+        self.dfs(ready_list, 0, S)
+        return self.result
+
+    def dfs(self, read_list, start, S):
+
+        if read_list in self.result:
+            return
+        self.result.append(read_list[:])
+
+        for i in range(start, len(S)):
+            self.dfs(read_list + [S[i]], i + 1, S)
