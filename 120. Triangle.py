00f86e2378504851c993e00e92542e83faf203cf
yuwenli <841843777@qq.com>
2017-08-15 11:35:06 +0800

    动态规划

---
+++ 120. Triangle.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def minimumTotal(self, triangle):
+        """
+        :type triangle: List[List[int]]
+        :rtype: int
+        """
+        n = len(triangle)
+        if n == 0:
+            return 0
+        dp = [[0] * i for i in range(1, n + 1)]
+
+        dp[0][0] = triangle[0][0]
+        for i in range(1, n):
+            #             处理两边的最小值
+            dp[i][0] = dp[i - 1][0] + triangle[i][0]
+            dp[i][i] = dp[i - 1][i - 1] + triangle[i][i]
+
+            for j in range(1, i):
+                dp[i][j] = min(dp[i - 1][j], dp[i - 1][j - 1]) + triangle[i][j]
+        print
+        dp
+        return min(dp[-1])
